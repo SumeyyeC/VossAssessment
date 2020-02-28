b77c8a441f702c1837aadca17742e2f53351a039
Samiye Cetintas <sumeyye.cetintas@yahoo.com>
2020-02-28 10:46:04 -0600

    done with authomation

--- src/test/java/com/MultiBrowserTest.java
+++ src/test/java/com/MultiBrowserTest.java
@@ -3,8 +3,8 @@ package com;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import com.github.javafaker.Faker;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -18,6 +18,9 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class MultiBrowserTest {
@@ -28,8 +31,8 @@ public class MultiBrowserTest {
 
     @Parameters("browserName")
     @BeforeMethod
-    public void setupMethod(String browserName) {
-        switch ("browserName") {
+    public void setupMethod(String browserName) throws IOException {
+        switch (browserName) {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
@@ -46,37 +49,40 @@ public class MultiBrowserTest {
         Assert.assertTrue(driver.getTitle().contains("Automation Practice - Ultimate QA"));
 
         //Taking screenshot
+        String time = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
         TakesScreenshot takesScreenshot = (TakesScreenshot)driver;
         File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
-        String target = System.getProperty("user.dir") + "/test-output/Screenshots" + ".png";
+        String target = System.getProperty("user.dir") + "/test-output/Screenshots" +time+ ".png";
         File finalDestination = new File(target);
+        FileUtils.copyFile(source, finalDestination);
 
         //2) Window maximizing
         driver.manage().window().maximize();
     }
 
-
     @Test
     public void test(){
-    //I signed up for a user first
-    //3) Login
-    driver.findElement(By.xpath("//div[@class='et_pb_text_inner']/ul/li[6]/a")).click();
-    driver.findElement(By.id("user[email]")).sendKeys(ConfigurationReader.getProperties("userEmail"));
-    driver.findElement(By.id("user[password]")).sendKeys(ConfigurationReader.getProperties("password"));
-    driver.findElement(By.xpath("//input[@type='submit']")).click();
-    Assert.assertTrue(driver.findElement(By.xpath("//a[@class='dropdown__toggle-button']")).getText().contains("John"));
+        //login part cancelled due to Captcha
+        // 5) Browse to "Fill out forms" page and complete all forms, followed by submit action
+      driver.findElement(By.xpath("//div[@class='et_pb_text_inner']/ul/li[4]/a")).click();
+      driver.findElement(By.xpath("//input[@id='et_pb_contact_name_0']")).sendKeys("John");
+      driver.findElement(By.xpath("//textarea[@id='et_pb_contact_message_0']")).sendKeys("My sons hero");
 
-    //4) Logout
-    driver.findElement(By.xpath("//i[@class='fa fa-caret-down']")).click();
-    driver.findElement(By.xpath("//ul[@class='dropdown__menu']/li[4]")).click();
-    //5) Navigating to "Fill out forms" page
-    driver.navigate().to(ConfigurationReader.getProperties("url"));
-    driver.findElement(By.xpath("//div[@class='et_pb_text_inner']/ul/li[4]/a")).click();
+      //left submit
+      driver.findElement(By.xpath("//*[@id='et_pb_contact_form_0']/div[2]/form/div/button")).click();
+      driver.findElement(By.xpath("//input[@id='et_pb_contact_name_1']")).sendKeys("Wick");
+      driver.findElement(By.xpath("//textarea[@id='et_pb_contact_message_1']")).sendKeys("My sons hero");
 
+      //right submit with addition
+      driver.findElement(By.xpath("//input[@class='input et_pb_contact_captcha']")).sendKeys("8");
+      driver.findElement(By.xpath("//*[@id='et_pb_contact_form_1']/div[2]/form/div/button")).click();
 
-
-
-
+      //6) Browse to the "Fake Pricing Page" and Purchase the Basic package
+      driver.navigate().to(ConfigurationReader.getProperties("url"));
+      Assert.assertTrue(driver.getTitle().contains("Automation Practice - Ultimate QA"));
+      driver.findElement(By.xpath("//div[@class='et_pb_text_inner']/ul/li[3]/a")).click();
+      driver.findElement(By.xpath("//*[@id=\"et-boc\"]/div/div/div[1]/div[2]/div[2]/div/div/div/div[4]/a")).click();
+      //after selecting basic package nothing happens
     }
 
     @AfterMethod
